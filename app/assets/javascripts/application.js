058c8cfb14ff25f2bc49b5de934ee8e37d504819
Tejas Rawal <tejasrawal@tejasmbp.local>
2015-09-23 00:59:42 -0500

    Make navbar stick to top on scroll. Styling changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,18 +16,53 @@
 //= require_tree .
 
 $(document).ready(function() {
+	bgFullPageWidth();
 	addMainImage();
+	mainNavUI();
+
+	window.onresize = function() {
+		bgFullPageWidth();
+	};
 });
 
 function addMainImage() {
-	var mainHeaderDiv = $('.add_main_image');
-
-	var img_src = "http://placehold.it/" + window.outerWidth + "x500",
-		imageHtml = '<img class="img-responsive" src=' + img_src + '>';
-
+	var mainHeaderDiv = $('.add_main_image'),
+		img_src = "http://placehold.it/" + window.outerWidth + "x500",
+		imageHtml = '<img class="img-responsive header_image" src=' + img_src + '>';
 
 	if (mainHeaderDiv) {
-//		mainHeaderDiv.append(imageHtml);
 		$(imageHtml).insertAfter(mainHeaderDiv);
 	}
+}
+
+function bgFullPageWidth() {
+	var bgDiv = $('.nav_dropdown_menu'),
+		windowWidth = window.outerWidth;
+
+	bgDiv.css('width', windowWidth);
+}
+
+function mainNavUI() {
+	var $window = $(window),
+		mainNavHolder = $('#main_navbar_holder'),
+		navBarLinks = $('.navbar_link');
+
+
+	navBarLinks.hover(function() {
+		$('.nav_dropdown_menu', this).slideToggle(100);
+	});
+
+	$window.scroll(function() {
+		var navDistanceFromTop = mainNavHolder.offset().top;
+
+		if ($window.scrollTop() >= navDistanceFromTop) {
+			mainNavHolder.css('position', 'fixed');
+			mainNavHolder.css('top', '0');
+			mainNavHolder.css('left', '0');
+		}
+
+		if(navDistanceFromTop === 0){
+			mainNavHolder.removeAttr('style');
+		}
+	});
 }
